1c636b77951efd50875b2f802741a71a2d9ba23a
2akia <2akssultana@gmail.com>
2024-02-08 21:33:46 +0000

    changed the title

--- src/components/Jumbotron.jsx
+++ src/components/Jumbotron.jsx
@@ -7,7 +7,7 @@ const Jumbotron = () => {
   <div className="jumbotron">
    <img src={coffeeImage} alt="Coffee Shop" className="jumbotron-image" />
    <div className="overlay-text">
-   <h1 className="display-4" >Welcome to Coffee Shop Reviews!</h1>
+   <h1 className="display-4" >Your Guide to Coffee Culture!</h1>
    <p className="lead">Discover the best coffee shops in town.</p>
    <button className="btn btn-primary btn-lg" href="#" role="button">Let's Go</button>
    </div>
