537305a05a034ac84d2be2cf3310f5961e1f6f5b
celiahouston <celia.houston99@gmail.com>
2024-02-08 20:30:38 +0000

    removed further data after checking out cafes

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,15 @@
 import * as React from 'react';
-import Card  from '@mui/material/Card';
-// import CardActions from '@mui/material/CardActions';
+// import { styled } from '@mui/material/styles';
+import Card from '@mui/material/Card';
+// import CardHeader from '@mui/material/CardHeader';
+// import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-// import Button from '@mui/material/Button';
+// import CardActions from '@mui/material/CardActions';
+// import Collapse from '@mui/material/Collapse';
+// import Avatar from '@mui/material/Avatar';
+// import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
+
 
 export default function CoffeeCard({ shopData }) {
   if (!shopData) {
@@ -15,7 +20,12 @@ export default function CoffeeCard({ shopData }) {
 
   return (
     <Card sx={{ maxWidth: 345 }}>
-      <CardContent>
+      <CardMedia
+        component="img"
+        height="194"
+        image=""
+        alt="" />
+        <CardContent>
         <Typography gutterBottom variant="h5" component="div">
           {BusinessName}
         </Typography>
