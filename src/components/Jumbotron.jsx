65175be5ad57e88cf5434a193f3596cf7ca2ebad
2akia <2akssultana@gmail.com>
2024-02-08 18:59:02 +0000

    added the images folder/ added jumbotron css folder

--- src/components/Jumbotron.jsx
+++ src/components/Jumbotron.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import coffeeImage from "./../assets/Image/coffee-jumbotron-img.jpg";
 
 const Jumbotron = () => {
  return (
   <div className="jumbotron">
+   <img src={coffeeImage} alt="Coffee Shop" className="jumbotron-image" />
+   <div className="overlay-text">
    <h2 className="display-4" >Welcome to Coffee Shop Reviews!</h2>
    <p className="lead">Discover the best coffee shops in town.</p>
    <button className="btn btn-primary btn-lg" href="#" role="button">Let's Go</button>
+   </div>
   </div>
  );
 };
