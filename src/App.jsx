9a7befb9db537147497926a88134ab7b050e2362
Gloal <alori.glo@gmail.com>
2024-02-12 04:41:46 +0000

    added favorite sidebar to map

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,10 @@ import Card from "./components/Card";
 /*import CardContainer from './components/CardContainer'; */
 import NavBar from "./components/NavBar";
 import Form from "./components/Form";
-//import Map from './components/Map';
 import Jumbotron from "./components/Jumbotron";
-// import coffee_shops from './data/london_coffee_shops.json';
 import londonRestaurantData from "./data/london_restaurants.json";
 import Footer from "./components/Footer.jsx";
-import MapComp from "./components/MapComp.jsx"
+import MapComp from "./components/MapComp.jsx";
 import cafes from "./data/london_cafes_reduced.json";
 
 function App() {
@@ -21,7 +19,6 @@ function App() {
   useEffect(() => {
     setRestaurantData(londonRestaurantData);
   }, []);
-
 
   return (
     <>
@@ -33,80 +30,10 @@ function App() {
         ))}
       </div>
 
-        <MapComp data={restaurantData} 
-          token ="pk.eyJ1IjoiZ2xvYWwiLCJhIjoiY2xzaTJobWpmMHFwMTJpcG5kampmdHZwbCJ9.d10-z1S4GiGH25co9SnrKw"
-          />
-
-
-
-{/*
-
-
-        <Map
-          initialViewState={{
-            longitude: -0.207,
-            latitude: 51.5,
-            zoom: 12,
-          }}
-          mapboxAccessToken={token}
-          style={{ width: 1200, height: 800, position: "center" }}
-          mapStyle="mapbox://styles/gloal/clsfe7wn0008d01qxh5p67hdn"
-
-          data={londonRestaurantData}
-         
-        >
-          {...londonRestaurantData.map((cafe) => (
-            <Marker
-              key={cafe._id}
-              latitude={cafe.Geocode_Latitude}
-              longitude={cafe.Geocode_Longitude}
-            >
-              {/*
-              <Button background="none"border="none" cursor="pointer" onClick={(e)=>{
-                e.preventDefault();
-                console.log(cafe);
-                setSelectedCafe(cafe);
-              }}>
-              <img src={pin} width="20px" height="30px" background="none" alt="cafe-icon"/>
-
-              </Button>
-
-
-              <img
-                src={pin}
-                width="25px"
-                height="30px"
-                background="none"
-                alt="cafe-icon"
-                cursor="pointer"
-                onClick={(e) => {
-                  e.preventDefault();
-                  setSelectedCafe(cafe);
-                  console.log(selectedCafe);
-                }}
-              />
-            </Marker>
-          ))}
-
-          {selectedCafe ? (
-            <Popup
-            height="200px" width="200px"
-            align-items="center"
-              longitude={selectedCafe.Geocode_Longitude}
-              latitude={selectedCafe.Geocode_Latitude}
-              closeOnClick={false}
-              onClose={() => {
-                setSelectedCafe(null);
-              }}>
-              <Card key={selectedCafe._id} shopId={selectedCafe._id} />
-            </Popup>
-          ):null}
-
-        </Map>
-      </div>
-
-
-            */}
+      <MapComp
+        data={restaurantData}
+        token="pk.eyJ1IjoiZ2xvYWwiLCJhIjoiY2xzaTJobWpmMHFwMTJpcG5kampmdHZwbCJ9.d10-z1S4GiGH25co9SnrKw"
+      />
 
       <Form />
       <Footer />
--- src/components/MapComp.jsx
+++ src/components/MapComp.jsx
@@ -3,59 +3,84 @@ import Map, { Marker, Popup } from "react-map-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 import pin from "../assets/Images/pin.svg";
 import Card from "./Card";
+import "../styles/MapComp.css";
+import { Link } from "@mui/material";
 
 const MapComp = ({ data, token }) => {
   const [selectedCafe, setSelectedCafe] = useState(null);
 
   return (
-    <Map
-      initialViewState={{
-        longitude: -0.207,
-        latitude: 51.5,
-        zoom: 12,
-      }}
-      mapboxAccessToken={token}
-      style={{ width: 1200, height: 800 }}
-      mapStyle="mapbox://styles/gloal/clsfe7wn0008d01qxh5p67hdn"
-    >
-      {...data.map((cafe) => (
-        <Marker
-          key={cafe._id}
-          latitude={cafe.Geocode_Latitude}
-          longitude={cafe.Geocode_Longitude}
-        >
-          <img
-            src={pin}
-            width="25px"
-            height="30px"
-            background="none"
-            alt="cafe-icon"
-            cursor="pointer"
-            onClick={(e) => {
-              e.preventDefault();
-              setSelectedCafe(cafe);
-              console.log(selectedCafe);
+    <>
+      <div className="wrap">
+        <div className="sidebar">
+          <div className="heading">
+            <h1>Favorite Cafes</h1>
+          </div>
+          <div id="listings" className="listings">
+            {...data.map((cafe) => (
+                <div className="item">
+                  <Link className="title" href="#">
+                    {cafe.BusinessName}
+                  </Link>
+                  <div>
+                    {cafe.AddressLine2} {cafe.PostCode}
+                  </div>
+                </div>
+            ))}
+          </div>
+        </div>
+        <div id="map" className="map">
+          <Map
+            initialViewState={{
+              longitude: -0.207,
+              latitude: 51.5,
+              zoom: 10,
             }}
-          />
-        </Marker>
-      ))}
+            mapboxAccessToken={token}
+            style={{ width: "75vw", height: "70vh" }}
+            mapStyle="mapbox://styles/gloal/clsfe7wn0008d01qxh5p67hdn"
+          >
+            {...data.map((cafe) => (
+              <Marker
+                key={cafe._id}
+                latitude={cafe.Geocode_Latitude}
+                longitude={cafe.Geocode_Longitude}
+              >
+                <img
+                  src={pin}
+                  width="25px"
+                  height="30px"
+                  background="none"
+                  alt="cafe-icon"
+                  cursor="pointer"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setSelectedCafe(cafe);
+                    console.log(selectedCafe);
+                  }}
+                />
+              </Marker>
+            ))}
 
-      {selectedCafe ? (
-        <Popup
-          height="200px"
-          width="200px"
-          align-items="center"
-          longitude={selectedCafe.Geocode_Longitude}
-          latitude={selectedCafe.Geocode_Latitude}
-          closeOnClick={false}
-          onClose={() => {
-            setSelectedCafe(null);
-          }}
-        >
-          <Card key={selectedCafe._id} shopId={selectedCafe._id} />
-        </Popup>
-      ) : null}
-    </Map>
+            {selectedCafe ? (
+              <Popup
+                height="200px"
+                width="200px"
+                align-items="center"
+                longitude={selectedCafe.Geocode_Longitude}
+                latitude={selectedCafe.Geocode_Latitude}
+                closeOnClick={false}
+                onClose={() => {
+                  setSelectedCafe(null);
+                }}
+              >
+                <Card key={selectedCafe._id} shopId={selectedCafe._id} />
+              </Popup>
+            ) : null}
+          </Map>
+        </div>
+      </div>
+    </>
   );
 };
 
